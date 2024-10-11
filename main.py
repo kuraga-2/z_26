2c765ca91a9d51cd934e22b36de956b26e20b432
kuraga-2 <denismanarskij@tutamail.com>
2024-10-11 17:26:30 +0300

    z_26

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from math import sqrt
 input_data = open('input.txt', 'r') 
 data = input_data.readlines()
 a = str(data[0])
 b = str(data[1])
 a = a.split()
 b = b.split()
-a1 = int(a[0])
-a2 = int(a[1])
-a3 = int(a[2])
-b1 = int(b[0])
-b2 = int(b[1])
-b3 = int(b[2])
+x1 = int(a[0])
+y1 = int(a[1])
+r1 = int(a[2])
+x2 = int(b[0])
+y2 = int(b[1])
+r2 = int(b[2])
 
 
-output_data = open('output.txt', 'w')
-output_data.write()
+if sqrt((x2 - x1)**2 + (y2 - y1)**2) <= r1 + r2 :
+    output_data = open('output.txt', 'w')
+    output_data.write('YES')
+else:
+    output_data = open('output.txt', 'w')
+    output_data.write('NO')
+
+
 input_data.close()
 output_data.close()
